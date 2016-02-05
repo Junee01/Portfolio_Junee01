5ef00fc92c1321bf1149ad05a351cf9b7813f522
Junee01 <potter777777@naver.com>
2016-02-06 03:39:43 +0900

    Setup & styled basic project

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,35 @@
+class ProjectsController < ApplicationController
+	before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@projects = Project.all.order("created_at desc")
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new project_params
+
+		if @project.save
+			redirect_to @project, notice: "프로젝트가 정상적으로 생성되었습니다."
+		else
+			render 'new'
+		end
+	end
+
+	def show
+
+	end
+
+	private
+
+	def find_project
+		@project = Project.find(params[:id])
+	end
+
+	def project_params
+		params.require(:project).permit(:title, :description, :link)
+	end
+end
