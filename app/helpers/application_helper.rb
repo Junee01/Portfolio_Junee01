e65e0783bd4954d4007941c70e71c1920e72f825
Junee01 <potter777777@naver.com>
2016-02-06 22:13:01 +0900

    redcarpet plus

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,12 +10,14 @@ module ApplicationHelper
 		renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
 		options = {
 			autolink: true,
-      no_intra_emphasis: true,
-      disable_indented_code_blocks: true,
-      fenced_code_blocks: true,
-      lax_html_blocks: true,
-      strikethrough: true,
-      superscript: true
+			no_intra_emphasis: true,
+			disable_indented_code_blocks: true,
+			fenced_code_blocks: true,
+			lax_html_blocks: true,
+			strikethrough: true,
+			superscript: true,
+			strikethrough: true,
+			highlight: true,
 		}
 		Redcarpet::Markdown.new(renderer, options).render(content).html_safe
 	end
