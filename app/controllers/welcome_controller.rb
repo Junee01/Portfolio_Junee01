2edfd9b611a3b8807c3152866074fc0617cd72ba
Junee01 <potter777777@naver.com>
2016-02-06 16:48:30 +0900

    Fixed some error

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 	def index
-		#최근에 작성된 3개의 글만 보여주도록 제한합니다.
-		@posts = Post.all.limit(3).order("created_at desc")
-		@projects = Project.all.limit(3).order("created_at desc")
+		#최근에 작성된 5개의 글만 보여주도록 제한합니다.
+		@posts = Post.all.limit(5).order("created_at desc")
+		@projects = Project.all.limit(5).order("created_at desc")
 	end
 end
