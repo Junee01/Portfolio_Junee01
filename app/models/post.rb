bea25afab78395e9bec20f9ba5506f76355ff973
Junee01 <potter777777@naver.com>
2016-02-09 02:06:05 +0900

    fixed disappear images

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 	extend FriendlyId
   	friendly_id :title, use: :slugged
 
-  	has_attached_file :image, styles: { medium: "400x400#" }
+  	has_attached_file :image
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
 	extend FriendlyId
   	friendly_id :title, use: :slugged
 
-  	has_attached_file :image, styles: { medium: "400x400#" }
+  	has_attached_file :image
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
