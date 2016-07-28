1a9baae76846addfac7e95cb0129ce04754a0e78
Junee01 <potter777777@naver.com>
2016-07-28 21:50:12 +0900

    welcome to 7

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 	def index
 		#최근에 작성된 5개의 글만 보여주도록 제한합니다.
-		@posts = Post.all.limit(5).order("created_at desc")
-		@projects = Project.all.limit(5).order("created_at desc")
+		@posts = Post.all.limit(7).order("created_at desc")
+		@projects = Project.all.limit(7).order("created_at desc")
 	end
 end
